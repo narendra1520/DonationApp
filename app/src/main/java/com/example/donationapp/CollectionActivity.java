2ca4d1f048864362bca1d1a3c3f954fb4d74e479
narendra1520 <46049530+narendra1520@users.noreply.github.com>
2020-01-21 00:59:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/donationapp/CollectionActivity.java
@@ -0,0 +1,15 @@
+package com.example.donationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class CollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+    }
+}
