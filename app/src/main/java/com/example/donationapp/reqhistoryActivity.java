9b253d6c8b271f20bb93dcb289f28503e920aa92
kajal2203 <60109135+kajal2203@users.noreply.github.com>
2020-01-23 20:21:31 -0800

    history

---
+++ app/src/main/java/com/example/donationapp/reqhistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.donationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reqhistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reqhistory);
+    }
+}
